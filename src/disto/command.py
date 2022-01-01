07b46c15ddc2f058757fd1bed797d2dd63bb144b
TwoPTwentySix <57607350+napstaa967@users.noreply.github.com>
2021-12-31 20:53:21 -0600

    Store commands more efficiently
    use dicts instead of args

--- src/disto/command.py
+++ src/disto/command.py
@@ -2,8 +2,8 @@ import inspect
 from . import client, posthandler
 
 global commandlist
-commandlist = []
-subcommandlist = []
+commandlist = {}
+subcommandlist = {}
 
 
 # https://stackoverflow.com/a/26151604
@@ -20,13 +20,17 @@ def parametrized(dec):
 
 class Commands:
     def __call__(self, commandstuff):
-        commandlist.append(commandstuff)
+        if not callable(commandstuff):
+            raise TypeError(f"Argument commandstuff should be 'Callable', not {type(commandstuff)}")
+        commandlist[commandstuff.__name__] = commandstuff
 
     @parametrized
     def subcommand(command, subcommandof):
-        subcommandlist.append({"upper": subcommandof, "lower": command})
-
-        # i hate python
+        if not callable(command):
+            raise TypeError(f"Argument command should be 'Callable', not {type(command)}")
+        if not isinstance(subcommandof, str):
+            raise TypeError(f"Argument subcommandof should be 'str', not {type(subcommandof)}")
+        subcommandlist[command.__name__] = {"upper": subcommandof, "lower": command}
 
     async def processcommands(self, messagecontent):
 
@@ -39,59 +43,56 @@ class Commands:
         if isbot is True:
             return
 
-        preargs = messagecontent["content"].split(" ")
-        premessage = preargs[0]
-        args = preargs
+        args = messagecontent["content"].split(" ")
+        message = args[0].removeprefix(client.prefix)
         del args[0]
-        message = premessage.removeprefix(client.prefix)
-        for command in commandlist:
-            cmd = str(command.__name__)
-            # Catch command args, check if subcommand, use subcommand if true
-            global torun
-            torun = command
-            global subcom
-            subcom = ""
-            for subc in subcommandlist:
-                if subc["upper"] == cmd and args[0] == subc["lower"].__name__:
-                    torun = subc["lower"]
-                    subcom = subc["upper"]
-            if (message == torun.__name__) or (message == subcom and args[0] == torun.__name__):
-                if len(args) > 0:
-                    if args[0] == torun.__name__:
-                        del args[0]
-                fullcmd = torun
-                topass = inspect.getfullargspec(torun).args
-                global usesargs
-                usesargs = False
-                global usesfullmessage
-                usesfullmessage = False
 
-                if "args" in topass:
-                    usesargs = True
-                if "msg" in topass:
-                    usesfullmessage = True
-                global ret_stuff
-                global alreadywaiting
-                alreadywaiting = False
-                if usesargs is True:
-                    if usesfullmessage is True:
-                        if alreadywaiting is False:
-                            ret_stuff = await fullcmd(args=args, msg=messagecontent)
-                            alreadywaiting = True
-                    if alreadywaiting is False:
-                        ret_stuff = await fullcmd(args=args)
-                        alreadywaiting = True
+        if commandlist[message] is not None:
+            global command
+            command = commandlist[message]
+            global hasremainingargs
+            hasremainingargs = False
+            try:
+                if args[0] is not None:
+                    hasremainingargs = True
+            except IndexError:
+                hasremainingargs = False
+            if hasremainingargs is True and subcommandlist[args[0]] is not None and subcommandlist[args[0]]["upper"] == commandlist[
+                message].__name__:
+                command = subcommandlist[args[0]]["lower"]
+                del args[0]
+            topass = inspect.getfullargspec(command).args
+            global usesargs
+            usesargs = False
+            global usesfullmessage
+            usesfullmessage = False
+
+            if "args" in topass:
+                usesargs = True
+            if "msg" in topass:
+                usesfullmessage = True
+            global ret_stuff
+            global alreadywaiting
+            alreadywaiting = False
+            if usesargs is True:
                 if usesfullmessage is True:
                     if alreadywaiting is False:
-                        ret_stuff = await fullcmd(msg=messagecontent)
+                        ret_stuff = await command(args=args, msg=messagecontent)
                         alreadywaiting = True
                 if alreadywaiting is False:
-                    ret_stuff = await fullcmd()
+                    ret_stuff = await command(args=args)
                     alreadywaiting = True
+            if usesfullmessage is True:
+                if alreadywaiting is False:
+                    ret_stuff = await command(msg=messagecontent)
+                    alreadywaiting = True
+            if alreadywaiting is False:
+                ret_stuff = await command()
+                alreadywaiting = True
 
-                if not callable(ret_stuff):
-                    return posthandler.createMessage(channel=messagecontent["channel_id"],
-                                                     content=ret_stuff)
+            if not callable(ret_stuff):
+                return posthandler.createMessage(channel=messagecontent["channel_id"],
+                                                 content=ret_stuff)
 
 
 command = Commands()
